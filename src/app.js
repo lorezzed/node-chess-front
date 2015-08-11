ac58ee69c05b1bc21a58ad49fc1ffb3d14439c7c
James Salamon <jjsalamon@gmail.com>
2015-08-11 18:58:21 +0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+'use strict';
+var angular = require("angular");
+var unique = require('uniq');
+var chess = require("node-chess");
+chess;
+var data = [1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2];
+console.log(unique(data));
+angular
+    .module('app', [])
+    .controller('myController', myController);
+function myController() {
+    this.sayHi = 'hiu';
+}
