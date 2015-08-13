d5ad4c499f3f925067d9c0c8b1c0ed46e87539f2
James <jjsalamon@gmail.com>
2015-08-13 23:23:13 +0800

    playing with parallax

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@
 var angular = require("angular");
 var unique = require('uniq');
 var chess = require("node-chess");
-chess;
+
+
+
 var data = [1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2];
 console.log(unique(data));
+
 angular
     .module('app', [])
-    .controller('myController', myController);
-function myController() {
-    this.sayHi = 'hiu';
-}
+    .controller('myController', require('./test/test.controller'));
+
+
+
+var debugInput = document.querySelector('input');
+debugInput.addEventListener('click', updateDebugState);
+updateDebugState();
+function updateDebugState() {
+    document.body.classList.toggle('debug-on', debugInput.checked);
+};
